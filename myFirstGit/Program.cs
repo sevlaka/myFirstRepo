bc832c3d62b4a76f6efbe53eaee15db6d3cb2b9a
Administrator <Administrator@COMP>
2022-08-08 17:16:30 +0300

    #  2 привета

--- myFirstGit/Program.cs
+++ myFirstGit/Program.cs
@@ -11,6 +11,7 @@ namespace myFirstGit
         static void Main(string[] args)
         {
             Console.WriteLine("Привет мир");
+            Console.WriteLine("Привет мир");
             Console.ReadKey();
         }
     }
